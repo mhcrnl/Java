2c3e6c7e70acf50925778c308a9dceacf532ee3b
Pan Otlet <otlet0@gmail.com>
2014-09-29 13:14:33 +0200

    taga

--- Start2/src/Zadanko.java
+++ Start2/src/Zadanko.java
@@ -4,25 +4,18 @@ public class Zadanko {
 
 	private static Scanner odczyt;
 
+	static Double a,b;
+	
 	public static void main(String[] args) {
 		System.out.println ("Podaj pierwsz¹ zmienn¹: ");
-		Double jeden;
 		odczyt = new Scanner(System.in);
-		jeden = odczyt.nextDouble();
+		a = odczyt.nextDouble();
 		System.out.println ("Podaj druga zmienn¹: ");
-		Double dwa;
-		odczyt = new Scanner(System.in);
-		dwa = odczyt.nextDouble();
-		Double dodawanie = jeden + dwa;
-		Double odejmowanie = jeden - dwa;
-		Double mnozenie = jeden * dwa;
-		Double dzielenie = jeden / dwa;
-		System.out.println ("Wynik dodawania: "+dodawanie);
-		System.out.println ("Wynik odejmowania: "+odejmowanie);
-		System.out.println ("Wynik mno¿enia: "+mnozenie);
-		System.out.println ("Wynik dzielenia: "+dzielenie);
-		
-
+		b = odczyt.nextDouble();
+		System.out.println ("Wynik dodawania: "+(a+b));
+		System.out.println ("Wynik odejmowania: "+(a-b));
+		System.out.println ("Wynik mno¿enia: "+(a*b));
+		System.out.println ("Wynik dzielenia: "+(a/b));
 	}
 
 }

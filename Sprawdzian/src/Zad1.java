a8445088088a9cab77a6c2c19f148e505fd46f81
matesik1995 <matesik1995@gmail.com>
2014-10-29 13:40:04 +0100

    Update Zad1.java

--- Sprawdzian/src/Zad1.java
+++ Sprawdzian/src/Zad1.java
@@ -6,14 +6,14 @@ public class Zad1 {
 	public static int a,b;
 
 	public static void main(String[] args) {
-		System.out.println ("Podaj pocz¹tek przedzia³u: ");
+		System.out.println ("Podaj poczatek przedzialu: ");
 		odczyt = new Scanner(System.in);
 		a = odczyt.nextInt();
-		System.out.println ("Podaj koniec przedzia³u: ");
-		odczyt = new Scanner(System.in);
+		System.out.println ("Podaj koniec przedzialu: ");
+		//odczyt = new Scanner(System.in);
 		b = odczyt.nextInt();
-		for (int i=a; i<b; i++){
-			if (i%3==0 && i%4==0){
+		for (int i=a; i<=b; i++){
+			if ((i%3==0) && (i%4==0)){
 				System.out.println(i);
 			}
 		}
